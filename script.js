2d7745c8f71c67d7596284611ae3399b29ccc75e
samuelkeitumetse-svg <samuelkeitumetse@gmail.com>
2026-07-13 14:03:57 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-alert("JavaScript loaded!");
+const beginButton = document.getElementById("beginButton");
+const nextButton = document.getElementById("nextButton");
+
+const cover = document.getElementById("cover");
+const page1 = document.getElementById("page1");
+const page2 = document.getElementById("page2");
+
+beginButton.addEventListener("click", function () {
+    cover.style.display = "none";
+
+    page1.classList.remove("hidden");
+    page1.classList.add("show");
+});
+
+nextButton.addEventListener("click", function () {
+    page1.classList.remove("show");
+    page1.classList.add("hidden");
+
+    page2.classList.remove("hidden");
+    page2.classList.add("show");
+});
