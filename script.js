0fe11eaa08b24e9d43d72280c2f1b44b368cdbc3
samuelkeitumetse-svg <samuelkeitumetse@gmail.com>
2026-07-13 18:10:47 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 const beginButton = document.getElementById("beginButton");
 const nextButton = document.getElementById("nextButton");
+const page2Button = document.getElementById("page2Button");
+const page3Button = document.getElementById("page3Button");
 
 const cover = document.getElementById("cover");
 const page1 = document.getElementById("page1");
 const page2 = document.getElementById("page2");
+const page3 = document.getElementById("page3");
+const page4 = document.getElementById("page4");
 
 beginButton.addEventListener("click", function () {
     cover.style.display = "none";
@@ -18,4 +22,20 @@ nextButton.addEventListener("click", function () {
 
     page2.classList.remove("hidden");
     page2.classList.add("show");
+});
+
+page2Button.addEventListener("click", function () {
+    page2.classList.remove("show");
+    page2.classList.add("hidden");
+
+    page3.classList.remove("hidden");
+    page3.classList.add("show");
+});
+
+page3Button.addEventListener("click", function () {
+    page3.classList.remove("show");
+    page3.classList.add("hidden");
+
+    page4.classList.remove("hidden");
+    page4.classList.add("show");
 });
