4c940c83eb7710e68975dab655305e7250ff1118
samuelkeitumetse-svg <samuelkeitumetse@gmail.com>
2026-07-13 13:48:34 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,1 @@
-const button = document.getElementById("beginButton");
-const cover = document.getElementById("cover");
-const page1 = document.getElementById("page1");
-
-button.addEventListener("click", function(){
-
-    cover.style.display = "none";
-
-    page1.classList.remove("hidden");
-
-    page1.classList.add("show");
-
-});
+alert("JavaScript loaded!");
